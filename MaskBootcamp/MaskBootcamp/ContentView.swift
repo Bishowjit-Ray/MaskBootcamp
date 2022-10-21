d3f0de59ba4aef13e09e97a15300db4171820fef
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2022-10-21 13:45:41 +0600

    Add files via upload

---
+++ MaskBootcamp/MaskBootcamp/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  MaskBootcamp
+//
+//  Created by Bishowjit Ray on 20/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var rating: Int = 0
+    
+    var body: some View {
+        
+      
+        ZStack {
+           starsView
+            
+                .overlay(overlayView.mask(starsView))
+            
+               
+        }
+    }
+    private var overlayView: some View {
+        GeometryReader { geometry in
+            ZStack{
+                Rectangle()
+                    //.foregroundColor(.yellow )
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.red,Color.blue ]), startPoint: .leading, endPoint: .trailing))
+                    .frame(width: CGFloat(rating) / 5 * geometry.size.width)
+            }
+            
+        }
+        .allowsHitTesting(false)
+    }
+    
+    private var starsView: some View{
+        HStack {
+            ForEach(1..<6)  { index in
+                
+                Image(systemName: "star.fill")
+                    .font(.largeTitle)
+                    .foregroundColor( Color.gray)
+                    .onTapGesture {
+                        withAnimation(.easeInOut){
+                            rating = index
+                        }
+                       
+                    }
+            }
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MaskBootcamp/MaskBootcamp/MaskBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaskBootcampApp.swift
+//  MaskBootcamp
+//
+//  Created by Bishowjit Ray on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MaskBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
